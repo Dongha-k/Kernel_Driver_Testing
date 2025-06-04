ea33dc714c4b744a3b1bd41ea932f2e2b69b69ed
dongha-k <regdla79@knu.ac.kr>
2025-06-04 17:44:50 +0900

    add custom ops

--- src/custom_main_ops.c
+++ src/custom_main_ops.c
@@ -7,4 +7,14 @@
 
 #include "../inc/custom_main_ops.h"
 
+u32 custom_driver_add(u32 a, u32 b)
+{
+    return a + b;
+}
+
+u32 custom_driver_mul(u32 a, u32 b)
+{
+    return a * b;
+}
+
 MODULE_LICENSE("GPL");
